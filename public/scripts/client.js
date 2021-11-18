c2707ac6f372bc7abbe6ef50c9df3cb08b722beb
Veronika Fatikhova <fidelio@list.ru>
2021-11-17 22:32:02 -0800

    Preventing XSS with Escaping implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,13 @@
  */
 $( document ).ready(function() {
 
+  //CORS: Preventing XSS with Escaping
+  const escape = (str) => {
+    let p = document.createElement("p");
+    p.appendChild(document.createTextNode(str));
+    return p.innerHTML;
+  };
+
   const createTweetElement = (tweet) => {
     const $tweet = $(`
       <article class="created-tweets">
@@ -13,7 +20,7 @@ $( document ).ready(function() {
           <span class="userid">${tweet.user.handle}</span>
         </header>
         
-        <p>${tweet.content.text}</p>
+        <p>${escape(tweet.content.text)}</p>
 
         <footer>
           <span>${timeago.format(tweet.created_at)}</span>
@@ -24,49 +31,44 @@ $( document ).ready(function() {
     return $tweet;
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   //Takes in an array of tweet objects and then appends each one to the .tweets-container
   const renderTweets = (tweets) => {
+    $('.tweets-container').empty(); //clear previous tweets before rendering the new one
     tweets.forEach(el => {
       const $tweet = createTweetElement(el); //calls createTweetElement for each tweet
-      $('.tweets-container').append($tweet); //appends returned value to the .tweets-container
+      $('.tweets-container').prepend($tweet); //appends returned value to the .tweets-container
     });
   };
-  renderTweets(data);
+
+  //Fetches tweets from the /tweets page - makes a request to /tweets and receive the array of tweets as JSON
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    })
+      .then(res => renderTweets(res));
+  };
+  loadTweets();
 
   //adds event lestener to the form
   $("form").on("submit", function(event) {
     event.preventDefault();
-    // creates an AJAX POST request and sends serialized data to the server as a query string
-    let url = $(this).attr("action"); // /tweets
-    console.log($(this).serialize());
-    //submits a POST request that sends the serialized data to the server
-    $.ajax({url, method: "POST", data: $(this).serialize()});// sterialize() turns a form input data into a query string
-  });
+    console.log($(".counter").val());
+    if ($(".counter").val() > 139) {
+      alert("Empty tweet");
+    } if ($(".counter").val() < 0) {
+      alert("Maximum characters exceeded");
+    } else {
+      //submits an AJAXPOST request that sends the serialized data to the server as a query string
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize(), // sterialize() turns a form input data into a query string
+        success: () => loadTweets()
+      });
+    }
+    //let url = $(this).attr("action"); // /tweets
 
- 
+    
+  });
 });
